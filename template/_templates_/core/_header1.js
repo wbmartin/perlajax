332f62b96561201efb8e962b2c63f6432bb3d504
wbmartin <w.brandon.martin@gmail.com>
2012-10-31 22:48:44 -0400

    js cleanup7~

--- template/_templates_/core/_header1.js
+++ template/_templates_/core/_header1.js
@@ -3,10 +3,10 @@ var usrSessionId = '';
 var usrLoginId = '';
 var usrLastAction = new Date();
 var usrLogoutScheduled = false;
-var usrTimeOutDuration = 20*60*1000;
+var usrTimeOutDuration = 20 * 60 * 1000;
 var clientLog = new Array();
 var insertUpdateChoose = 'INSERTUPDATE';
-var FAILF = function(){alert('FAIL');}
+var FAILF = function() {alert('FAIL');};
 var currentContentPane = '';
 var SERVER_SIDE_FAIL = 'serverSideFail';
 var OUTSTANDING_SERVER_CALLS = 0;
@@ -15,50 +15,52 @@ var CURRENT_PAGE = '';
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //Form Functions
-function appendValidationMsg(formId,fieldId, msg){
-	var fullyQName = 'form#'+formId+' #' + fieldId;
-	if ($('form#'+formId).find('#'+fieldId+'Error').attr('id') == undefined) {
-		$(fullyQName).after('<span id='' +fieldId + 'Error' class="ValidationMsg"></span>');
+function appendValidationMsg(formId, fieldId, msg) {
+	var fullyQName = 'form#' + formId + ' #' + fieldId;
+	if ($('form#' + formId).find('#' + fieldId + 'Error').attr('id') == undefined) {
+		var tmpSpan = '<span id = "' + fieldId;
+	 	tmpSpan += 'Error" class = "ValidationMsg"></span>';
+		$(fullyQName).after(tmpSpan);
 	}
-	if ( $(fullyQName+'Error').html().length ==0) {
-		$(fullyQName+'Error').append('*');
+	if ($(fullyQName + 'Error').html().length == 0) {
+		$(fullyQName + 'Error').append('*');
 	}
-	$(fullyQName+'Error').append(' ' + msg);
+	$(fullyQName + 'Error').append(' ' + msg);
 	$(fullyQName).addClass('invalid');
 }
 
 function highlightFieldError(formId, fieldId, yesNo) {
-	var bordercolor='black';
-	if (yesNo) bordercolor='red'
-		$('form#'+formId+' #'+fieldId).css('border-color:'+bordercolor);
+	var bordercolor = 'black';
+	if (yesNo) bordercolor = 'red';
+		$('form#' + formId + ' #' + fieldId).css('border-color:' + bordercolor);
 
 }
 
 function bindForm(formName) {
 	var rslt = {};
 	var fieldId = '';
-	$.each($('form#'+formName+' :input'),
-			function(key, field){
-				fieldId = field.id.replace(formName+'-','');
-				if (field.type != 'button') rslt[fieldId] = field.value
+	$.each($('form#' + formName + ' :input'),
+			function(key, field) {
+				fieldId = field.id.replace(formName + '-', '');
+				if (field.type != 'button') rslt[fieldId] = field.value;
 			});
 	return rslt;
 }
 
-function bindToForm(formName, obj){
+function bindToForm(formName, obj) {
 	var fieldId = '';
-	$.each($('form#'+formName+' :input'),
-			function(key, field){
-				fieldId = field.id.replace(formName+'-','');
-				if (field.type != 'button')  field.value = obj[fieldId];
+	$.each($('form#' + formName + ' :input'),
+			function(key, field) {
+				fieldId = field.id.replace(formName + '-', '');
+				if (field.type != 'button') field.value = obj[fieldId];
 			});
 }
 
-function clearForm(formName){
+function clearForm(formName) {
 	var fieldId = '';
-	$.each($('form#'+formName+' :input'),
-			function(key, field){
-				fieldId = field.id.replace(formName+'-','');
+	$.each($('form#' + formName + ' :input'),
+			function(key, field) {
+				fieldId = field.id.replace(formName + '-', '');
 				if (field.type == 'checkbox') {
 					field.checked = false;
 				} else if (field.type != 'button') field.value = '';
@@ -67,55 +69,50 @@ function clearForm(formName){
 	toggleSaveMode(formName, false);
 }
 
-function toggleSaveMode(formName, saveMode){
-	var buttonToShow = (saveMode)?'Save':'Add';
-	var buttonToHide = (!saveMode)?'Save':'Add';
-	$('form#'+formName+' #' +formName +buttonToHide).addClass('LogicDisabled');
-	$('form#'+formName+' #' +formName +buttonToShow).removeClass('LogicDisabled');
-
-
-
+function toggleSaveMode(formName, saveMode) {
+	var buttonToShow = (saveMode) ? 'Save' : 'Add';
+	var buttonToHide = (!saveMode) ? 'Save' : 'Add';
+	var id = 'form#' + formName + ' #' + formName + buttonToHide;
+	$(id).addClass('LogicDisabled');
+	id = 'form#' + formName + ' #' + formName + buttonToShow;
+	$(id).removeClass('LogicDisabled');
 }
 
-
-
-function standardValidate(formName){
+function standardValidate(formName) {
 	var formValid = true;
-	if ($('#'+formName).length==0) formValid=false;
-	$.each($('form#'+formName + ' span.ValidationMsg'),function (ndx,span){
-		span.innerHTML='';
+	if ($('#' + formName).length == 0) formValid = false;
+	$.each($('form#' + formName + ' span.ValidationMsg'), function(ndx, span) {
+		span.innerHTML = '';
 	});
-	$.each($('form#'+formName + ' .invalid'),function (ndx,field){
-		$('form#'+formName +' #'+field.id).removeClass('invalid');
+	$.each($('form#' + formName + ' .invalid'), function(ndx, field) {
+		$('form#' + formName + ' #' + field.id).removeClass('invalid');
 	});
-	$.each($('form#' +formName + ' .VALIDATErequired'),function (ndx,field){
-		if (field.value == null || field.value==''){
-			appendValidationMsg(formName,field.id, 'Required');
-			highlightFieldError(formName,field.id,true);
-			formValid =false;
+	$.each($('form#' + formName + ' .VALIDATErequired'), function(ndx, field) {
+		if (field.value == null || field.value == '') {
+			appendValidationMsg(formName, field.id, 'Required');
+			highlightFieldError(formName, field.id, true);
+			formValid = false;
 		}
 	});
-	$.each($('form#' +formName + ' .VALIDATEinteger'),function (ndx,field){
-		if (field.value != null && !isInteger(field.value)){
-			appendValidationMsg(formName,field.id, 'Integer Input Required');
-			highlightFieldError(formName,field.id,true);
-			formValid =false;
+	$.each($('form#' + formName + ' .VALIDATEinteger'), function(ndx, field) {
+		if (field.value != null && !isInteger(field.value)) {
+			appendValidationMsg(formName, field.id, 'Integer Input Required');
+			highlightFieldError(formName, field.id, true);
+			formValid = false;
 		}
 	});
-	$.each($('form#' +formName + ' .VALIDATEmmddyyyydate'),function (ndx,field){
-		if (field.value != null && !field.value.match(/\d\d\/\d\d\/\d\d\d\d/)){
-			appendValidationMsg(formName,field.id, 'MM/DD/YYYY Required');
-			highlightFieldError(formName,field.id,true);
-			formValid =false;
-		}
+	$.each($('form#' + formName + ' .VALIDATEmmddyyyydate'),
+			function(ndx, field) {
+				if (field.value != null && !field.value.match(/\d\d\/\d\d\/\d\d\d\d/)) {
+					appendValidationMsg(formName, field.id, 'MM/DD/YYYY Required');
+					highlightFieldError(formName, field.id, true);
+					formValid = false;
+				}
 	});
-
-
-
 	return formValid;
 }
 
-function isFieldIdEmpty(fieldId_){
+function isFieldIdEmpty(fieldId_) {
 	if (document.getElementById(fieldId_) == undefined) return true;
 	if (document.getElementById(fieldId_).value == undefined) return true;
 	if (document.getElementById(fieldId_).value == null) return true;
@@ -123,14 +120,14 @@ function isFieldIdEmpty(fieldId_){
 	return false;
 }
 
-function isEmpty(val){
-	if (typeof(val) == 'undefined')return true;
-	if (val == null || val =='') return true;
+function isEmpty(val) {
+	if (typeof(val) == 'undefined') return true;
+	if (val == null || val == '') return true;
 	return false;
 }
 
-function isInteger(value){
-	if ((parseFloat(value) == parseInt(value)) && !isNaN(value)){
+function isInteger(value) {
+	if ((parseFloat(value) == parseInt(value)) && !isNaN(value)) {
 		return true;
 	} else {
 		return false;
@@ -140,50 +137,50 @@ function isInteger(value){
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //Formatting Functions
-function pgDate(val){
+function pgDate(val) {
 	var rslt;
-	if (val !=null) {
-		rslt = val.substring(5,7) + '/'+val.substring(8,10) + '/' + val.substring(0,4);
+	if (val != null) {
+		rslt = val.substring(5, 7) + '/' + val.substring(8, 10) + '/' + val.substring(0, 4);
 	} else {
-		rslt= '';
+		rslt = '';
 	}
 	return rslt;
 }
 
-function formatNumber(num,decimalNum,bolLeadingZero,bolParens,bolCommas) {
+function formatNumber(num, decimalNum, bolLeadingZero, bolParens, bolCommas) {
 	if (isNaN(parseInt(num))) return '---';
 	var tmpNum = num;
 	var iSign = num < 0 ? -1 : 1;		// Get sign of number
-	tmpNum *= Math.pow(10,decimalNum);
-	tmpNum = Math.round(Math.abs(tmpNum))
-		tmpNum /= Math.pow(10,decimalNum);
-	tmpNum *= iSign;						
+	tmpNum *= Math.pow(10, decimalNum);
+	tmpNum = Math.round(Math.abs(tmpNum));
+	tmpNum /= Math.pow(10, decimalNum);
+	tmpNum *= iSign;
 	var tmpNumStr = new String(tmpNum);
 
 	// See if we need to strip out the leading zero or not.
 	if (!bolLeadingZero && num < 1 && num > -1 && num != 0)
-		if (num > 0)tmpNumStr = tmpNumStr.substring(1,tmpNumStr.length);
-		else tmpNumStr = '-' + tmpNumStr.substring(2,tmpNumStr.length);
+		if (num > 0) tmpNumStr = tmpNumStr.substring(1, tmpNumStr.length);
+		else tmpNumStr = '-' + tmpNumStr.substring(2, tmpNumStr.length);
 
 	// See if we need to put in the commas
 	if (bolCommas && (num >= 1000 || num <= -1000)) {
 		var iStart = tmpNumStr.indexOf('.');
-		if (iStart < 0)	iStart = tmpNumStr.length;
+		if (iStart < 0) iStart = tmpNumStr.length;
 
 		iStart -= 3;
 		while (iStart >= 1) {
-			tmpNumStr = tmpNumStr.substring(0,iStart) + ',' + tmpNumStr.substring(iStart,tmpNumStr.length)
+			tmpNumStr = tmpNumStr.substring(0, iStart) + ',' + tmpNumStr.substring(iStart, tmpNumStr.length);
 				iStart -= 3;
-		}		
+		}
 	}
 
 	// See if we need to use parenthesis
-	if (bolParens && num < 0) tmpNumStr = '(' + tmpNumStr.substring(1,tmpNumStr.length) + ')';
+	if (bolParens && num < 0) tmpNumStr = '(' + tmpNumStr.substring(1, tmpNumStr.length) + ')';
 
-	if (tmpNumStr.indexOf('.')<0 && decimalNum >0) {
+	if (tmpNumStr.indexOf('.') < 0 && decimalNum > 0) {
 		tmpNumStr += '.';
 	}
-	while ((tmpNumStr.length -tmpNumStr.indexOf('.')) <= decimalNum) {
+	while ((tmpNumStr.length - tmpNumStr.indexOf('.')) <= decimalNum) {
 		tmpNumStr += '0';
 
 	}
@@ -192,7 +189,7 @@ function formatNumber(num,decimalNum,bolLeadingZero,bolParens,bolCommas) {
 }
 
 
-String.prototype.ucfirst = function () {
+String.prototype.ucfirst = function() {
 	// Split the string into words if string contains multiple words.
 	var x = this.split(/\s+/g);
 	for (var i = 0; i < x.length; i++) {
@@ -210,7 +207,7 @@ String.prototype.ucfirst = function () {
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //js Utility Functions
-function filterCacheArrayByVal (cacheArray, idToSet) {
+function filterCacheArrayByVal(cacheArray, idToSet) {
 	for (var i = 0; i < cacheArray.length; i++) {
 		if (cacheArray[i].val == idToSet)return cacheArray[i];
 	}
@@ -234,9 +231,9 @@ function deepCopy(obj) {
 }
 
 function setSelectOptions(selectId, obj) {
-	var newhtml='<option value=""></option>';
-	$.each(obj,function(key, val) {
-		newhtml += '<option value="' + key + '">' + val +'</option>';
+	var newhtml = '<option value=""></option>';
+	$.each(obj, function(key, val) {
+		newhtml += '<option value="' + key + '">' + val + '</option>';
 	});
 	$(selectId).html(newhtml);
 
@@ -245,14 +242,16 @@ function setSelectOptions(selectId, obj) {
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //Content Functions
-function timeoutIfNoAction() { 'use strict';
-	var timeSince = new Date().getTime() - usrLastAction.getTime() ;
-	if (usrLoginId !='' && timeSince >usrTimeOutDuration ) {
+function timeoutIfNoAction() {
+	var timeSince = new Date().getTime() - usrLastAction.getTime();
+	if (usrLoginId != '' && timeSince > usrTimeOutDuration) {
 		usrLogoutScheduled = true;
 		statusMsg('Logging Out User in 1 minute');
-		window.setTimeout(function(){if (usrLogoutScheduled) {logOutUser();}},60000);
+		window.setTimeout(function() {
+			if (usrLogoutScheduled) {logOutUser();}
+		}, 60000);
 	} else {
-		window.setTimeout(timeoutIfNoAction, usrTimeOutDuration +1000  );
+		window.setTimeout(timeoutIfNoAction, usrTimeOutDuration + 1000);
 	}
 
 }
@@ -281,31 +280,31 @@ function registerAction() {
 	usrLogoutScheduled = false;
 }
 
-window.onbeforeunload = function () {
-	if (usrSessionId !='') {
+window.onbeforeunload = function() {
+	if (usrSessionId != '') {
 		var msg = 'If you click OK and continue to refresh this page, ';
 		msg += ' you will lose any data that has not been saved';
 		return msg;
 	}
-}
+};
 
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //Log Msg Functions
 function statusMsg(msg) {
 	$('#statusMsg').html(msg);
-	logMsg('Console Msg:' +msg);
+	logMsg('Console Msg:' + msg);
 }
-function logMsg(msg,requestId) {
-	var timingInfo='';
+function logMsg(msg, requestId) {
+	var timingInfo = '';
 	var logId = clientLog.length;
 	if (requestId != null && requestId != 'NEW') { logId = requestId; }
 	if (logId == clientLog.length) {
 		clientLog[logId] = {logDt: new Date()};
 	} else {
-		timingInfo = ' | timing: ' + (new Date() -  clientLog[logId].logDt) + 'ms.';
+		timingInfo = ' | timing: ' + (new Date() - clientLog[logId].logDt) + 'ms.';
 	}
-	clientLog[logId].msg= msg+timingInfo;
+	clientLog[logId].msg = msg + timingInfo;
 	return logId;
 }
 
@@ -320,22 +319,23 @@ function serverCall(params, successCallback, failCallback) {
 		decrementServerCalls();
 		logMsg(resourceActionInfo + ' Responded', rslt.requestId);
 		if (!validateServerResponse(rslt)) {
-			var msg = resourceActionInfo + ' Failed to validate the server response - ';
+			var msg = resourceActionInfo;
+		  msg += 'Failed to validate the server response - ';
 			msg += rslt['errorMsg'];
 			logMsg(msg);
 			rslt[SERVER_SIDE_FAIL] = true;
 		} else {
 			rslt[SERVER_SIDE_FAIL] = false;
-		}	 
+		}
 		successCallback(rslt);
-	}
+	};
 	incrementServerCalls();
 	$.ajax({type: 'POST',
 		url: urlTarget,
 		dataType: 'json',
 		data: params,
 		success: successCallbackMod,
-		error: failCallback 
+		error: failCallback
 	});
 }
 
@@ -345,8 +345,8 @@ function incrementServerCalls() {
 }
 
 function decrementServerCalls() {
-	OUTSTANDING_SERVER_CALLS = (--OUTSTANDING_SERVER_CALLS <0) ?
-		0:OUTSTANDING_SERVER_CALLS;
+	OUTSTANDING_SERVER_CALLS = (--OUTSTANDING_SERVER_CALLS < 0) ?
+		0 : OUTSTANDING_SERVER_CALLS;
 	var msg = 'Open Requests: ' + OUTSTANDING_SERVER_CALLS;
 	$('#outstandingServerCalls').html(msg);
 }
@@ -358,18 +358,18 @@ function handleServerResponse(msg, startTime, data) {
 		statusMsg('Server Communication Error');
 		return false;
 	}
-	var timing = (new Date().getTime()-startTime.getTime())/1000;
-	statusMsg( msg + ' in ' + timing + 's' );
+	var timing = (new Date().getTime() - startTime.getTime()) / 1000;
+	statusMsg(msg + ' in ' + timing + 's');
 	return true;
 }
 
 function prepParams(params, resource, action) {
-	if (params == null) { params = {}; }
+	if (params == null) {params = {};}
 	params['spwfResource'] = resource;
 	params['user_id'] = usrLoginId;
 	params['session_id'] = usrSessionId;
 	if (action == insertUpdateChoose) {
-		if (params['last_update']!= null && params['last_update'] != '') {
+		if (params['last_update'] != null && params['last_update'] != '') {
 			action = 'update';
 		} else {
 			action = 'insert';
@@ -380,18 +380,18 @@ function prepParams(params, resource, action) {
 }
 
 function validateServerResponse(responseTxt) {
-	if (responseTxt == undefined || responseTxt == null ) {
+	if (responseTxt == undefined || responseTxt == null) {
 		logMsg('validateServerResponse - undefined response');
 		var msg = 'Sorry, there was an unexpected error ';
 		msg += 'communicating with the server.';
 		alert(msg);
 		return false;
 	} else if (responseTxt.errorMsg != undefined) {
-		if (responseTxt.errorMsg.indexOf('Session Invalid') > -1 ) {
+		if (responseTxt.errorMsg.indexOf('Session Invalid') > -1) {
 			alert('Sorry, the session has expired and you will be logged out');
 			usrSessionId = '';
 		}
-		logMsg ('validateServerResponse - Error Msg: ' + responseTxt.errorMsg);
+		logMsg('validateServerResponse - Error Msg: ' + responseTxt.errorMsg);
 		return false;
 	}
 	registerAction();
